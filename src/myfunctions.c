45aa236839692c4c0fa1dd49939fdb1e2c8a4a83
Marc Cremer <marc@cremer.dev>
2019-12-10 05:08:12 +0100

    removed comments

--- src/myfunctions.c
+++ src/myfunctions.c
@@ -1,13 +1,12 @@
 /*myfunctions.c */
 #include "myfunctions.h"
 
+
 int sucheZeichen(const char str[], char c){
-	//int length = strlen(str);
 	int foundchars = 0;
 	for(int i=0; str[i] != '\0'; i++){
 		if (str[i] == c )
 		{
-			//printf("found\n");
 			foundchars += 1;
 		}
 	}
